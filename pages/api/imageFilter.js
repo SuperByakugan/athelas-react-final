ca68cd79c2f710be45673cdf6a61e38c60d7a8e7
SuperByakugan <43621807+SuperByakugan@users.noreply.github.com>
2022-07-28 16:47:58 -0700

    adding files

--- pages/api/imageFilter.js
+++ pages/api/imageFilter.js
@@ -1,23 +1,29 @@
-//import { readSync } from 'fs';
-
-/*const filter = require('node-image-filter');
-const FormData = require('form-data');
-const fs = require('fs')
-const os = require('os')
-const path = require('path')*/
+//Importing sharp for compression and filter
 const sharp = require('sharp');
+//Importing reqest to get image from web
 var request = require('request').defaults({ encoding: null });
 
 export default async function handle(req, res)
 {
+    //Extracting url from reqest and adding .jpg at the end as good practice
     let imageUrl = String(req.body.imgUrl) + ".jpg";
+    //Fetching the image using requests
+    //Can't just use imgUrl because sharp assumes the url is a 
+    //JPG/asdk123mdfdald-?./jskjd instead of a JPG because of how the url is formated
     request.get(imageUrl, function (error, response, body)
     {
+        //checks if there is no reqest error
         if (!error && response.statusCode == 200) {
+            //image stored in data
             let data = body;
+            //Constructs sharp obj and uses the grey scale function to turn it
+            //to black and white
+            //Uses to Buffer so we can convert it to base 64
             sharp(data).greyscale().toBuffer().then((put) =>
             {
+                //convert to base 64
                 let a = put.toString('base64');
+                //return base 64 in json format
                 res.send
                 ({
                     fileData: a
@@ -26,6 +32,16 @@ export default async function handle(req, res)
         }
     });
 }
+
+
+//import { readSync } from 'fs';
+
+/*const filter = require('node-image-filter');
+const FormData = require('form-data');
+const fs = require('fs')
+const os = require('os')
+const path = require('path')*/
+
 
 // upload image -> send image to server -> server add metadata -> send to firebase ->
 // send image to server to apply filter -> return image
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import styles from '../styles/Home.module.css';
+//imports
 import React from 'react';
 import {storage} from "../firebase.js";
 import {ref, uploadBytes, getDownloadURL} from 'firebase/storage';
@@ -7,24 +7,36 @@ import axios from 'axios';
 
 export default function Home()
 {
+  //Set states for initial file upload and image displayed on webpage
   const [file, setFile] = React.useState(null);
   const [img, setImg] = React.useState(null);
-  //const [blackAndWhite, setBW] = React.useState("JFIF Image");
-  
+  //Function that runs when the button is clicked
   const uploadImage = () =>
   {
-    console.log(storage);
+    //Checking if there is a file
     if(file == null)return;
+    console.log(storage);
+    //Where to place the image in firebase storage
     const imageRef = ref(storage, `images/${file.name + v4()}`);
+    //Uploading image to database w promise
     uploadBytes(imageRef, file).then(() =>
     {
+      //Once it's uploaded, we can get the firebase url to the image w promise
       getDownloadURL(imageRef)
       .then(link => {
         console.log(link);
+        //Now that we've recieved the link to the image, we're going to display the html
+        //image tag and set the img state to this link
+        //It will re-render the image so that it now displays
+        //This is a good for the client because they can have an image before the enpoint
+        //returns their filtered image
+        //Also good for debugging
         setImg(function(){
           document.getElementById("firstImg").style.display = "block";
           return link;
         });
+        //Requesting the imageFilter endpoint with the link to our image to
+        //Get a black and white image in return
         axios.post('http://localhost:3000/api/imageFilter',
         {
           responseType: 'blob',
@@ -32,6 +44,10 @@ export default function Home()
         })
         .then((res) =>
         {
+          //Once we get the response from the server, we need to display it
+          //It's in base64 format so to display it, we need to add the data:img.. 
+          //information before the link
+          //We set img state to this updated link
           console.log("data:image/png;base64," + res.data.fileData);
           setImg(() => {
             return "data:image/png;base64," + res.data.fileData;
@@ -43,11 +59,17 @@ export default function Home()
 
   return (
     <div id = "wrapper">
-      <div id="form-container">
-        <h1>Upload Image</h1>
-        <label htmlFor = "imgUploadObj" id="imgUploadObjLabel">Choose a file</label>
-        <input onChange = {(e) => setFile(e.target.files[0])} type = "file" id = "imgUploadObj"/>
-        <button onClick = {uploadImage}>Upload Image</button>
+      <div id = "form-wrapper">
+        <div id="form-container">
+          <h1>UPLOAD IMAGE</h1>
+          {/*Replacing upload with this icon*/}
+          <label htmlFor = "imgUploadObj" id="imgUploadObjLabel">
+            <img src = "../renameUpload.png" id="limg"/>
+          </label>
+          {/*Changing file state whenver the file is changed*/}
+          <input onChange = {(e) => setFile(e.target.files[0])} type = "file" id = "imgUploadObj"/>
+          <button onClick = {uploadImage} id ="imgSubmit">Upload Image</button>
+        </div>
       </div>
       <div>
         <img alt="black and white" id="firstImg" src={img}/>
